340fbf11c206e2e2ce14fbde0f0244b603bec7c2
henrikbreivik <henrik.breivik@quantafuel.com>
2025-02-04 14:25:06 +0100

    test_Henke

--- Test_function_Henrik/handler.py
+++ Test_function_Henrik/handler.py
@@ -1,10 +1,16 @@
-# -*- coding: utf-8 -*-
-# """
-# Created on Tue Feb  4 12:52:49 2025
-#
-# @author: Henrik.Rost.Breivik
-# """
+from common.utilities import sum_integers
 
 
-def handle(client):
-    print("Hello henr")
+def handle(data, client):
+    print("I got the following data:")
+    print(data)
+
+    if not ("a" in data and "b" in data):
+        raise KeyError("Data should contain both keys: 'a' and 'b'")
+
+    data["sum"] = sum_integers(data["a"], data["b"])
+
+    print("Hello world")
+    print("Will now return updated data")
+
+    return data
