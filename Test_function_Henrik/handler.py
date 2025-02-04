080fb952f50997ce9fa28e8878e3b290cabf03b2
henrikbreivik <henrik.breivik@quantafuel.com>
2025-02-04 14:04:07 +0100

    test_f0unction_Henke

--- Test_function_Henrik/handler.py
+++ Test_function_Henrik/handler.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-#"""
-#Created on Tue Feb  4 12:52:49 2025
+# """
+# Created on Tue Feb  4 12:52:49 2025
 #
-#@author: Henrik.Rost.Breivik
-#"""
+# @author: Henrik.Rost.Breivik
+# """
+
 
 def handle(client):
     print("Hello henr")
